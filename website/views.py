14ecb7c3273d8225841d72a23b926bea9b44e079
ViCastorino <vih.teodoro16@gmail.com>
2019-07-25 16:58:58 -0300

    criar models, form, doc

--- website/views.py
+++ website/views.py
@@ -3,13 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    args = {
+    args = {}
 
-    }
+    if request.method == 'POST':
+        pessoa = Pessoa()
+        pessoa.nome = request.POST.get('nome')
+        pessoa.sobrenome = request.POST.get('sobrenome')
+        pessoa.email = request.POST.get('email')
+        pessoa.genero = request.POST.get('genero')
+        pessoa.save()
+        args('msg': 'usuario cadastrado')
+
     return render(request, 'index.html', args)
 
 def sobre(request):
+    pessoa = Pessoa.objects.all()
     args = {
-
+        'pessoas':pessoa
     }
     return render(request, 'sobre.html', args)
